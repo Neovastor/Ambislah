147acbdc8cca2e1c012814b6bdd94e2e88a6937b
fadilaharifki <fadilah.arifki29@gmail.com>
2021-07-23 16:44:04 +0700

    create page

--- client/src/pages/Create.js
+++ client/src/pages/Create.js
@@ -1,14 +1,19 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faVolumeUp, faCheck } from '@fortawesome/free-solid-svg-icons';
 
 export default function Create() {
+
     return (
         <section>
-            <div className="grid grid-cols-8 pt-12">
-                <div className="bg-gray-200 my-2 p-2 col-span-7">
+            <div className="pt-12 md-max:flex md-max:flex-col-reverse">
+                <div className="bg-gray-200 my-2 p-2 col-span-7 h-screen">
                     <form className="flex flex-col gap-y-4 items-center p-5">
                         <input placeholder="Input question title" className="w-full px-4 py-2 transition duration-300 border border-gray-300 rounded focus:border-transparent focus:outline-none focus:ring-4 focus:ring-green-400" />
                         <input placeholder="Start typing your question" className="w-full px-4 py-2 transition duration-300 border border-gray-300 rounded focus:border-transparent focus:outline-none focus:ring-4 focus:ring-green-400" />
-
+                        <button>
+                            <FontAwesomeIcon icon={faVolumeUp}></FontAwesomeIcon>
+                        </button>
                         <div className="grid grid-cols-1 mt-5 mx-7">
                             <div className="flex items-center justify-center w-full">
                                 <label className="flex flex-col border-4 border-dashed w-96 h-32 hover:bg-gray-100 border-green-300 group">
@@ -25,24 +30,23 @@ export default function Create() {
                         <div className="grid grid-cols-2 gap-4">
                             <div className="flex">
                                 <input placeholder="add answer 1" className="w-full px-4 py-2 transition duration-300 border border-gray-300 rounded focus:border-transparent focus:outline-none focus:ring-4 focus:ring-green-400" />
-                                <button className="rounded-full p-2 w-10 hover:bg-green-400 text-white"> &#10004; </button>
+                                <button className="rounded-full p-2 w-10 hover:bg-green-400 text-gray-200"><FontAwesomeIcon icon={faCheck}></FontAwesomeIcon></button>
                             </div>
                             <div className="flex">
                                 <input placeholder="add answer 2" className="w-full px-4 py-2 transition duration-300 border border-gray-300 rounded focus:border-transparent focus:outline-none focus:ring-4 focus:ring-green-400" />
-                                <button className="rounded-full p-2 w-10 hover:bg-green-400 text-white"> &#10004; </button>
+                                <button className="rounded-full p-2 w-10 hover:bg-green-400 text-gray-200"> <FontAwesomeIcon icon={faCheck}></FontAwesomeIcon> </button>
                             </div>
                             <div className="flex">
                                 <input placeholder="add answer 3" className="w-full px-4 py-2 transition duration-300 border border-gray-300 rounded focus:border-transparent focus:outline-none focus:ring-4 focus:ring-green-400" />
-                                <button className="rounded-full p-2 w-10 hover:bg-green-400 text-white"> &#10004; </button>
+                                <button className="rounded-full p-2 w-10 hover:bg-green-400 text-gray-200"> <FontAwesomeIcon icon={faCheck}></FontAwesomeIcon> </button>
                             </div>
                             <div className="flex">
                                 <input placeholder="add answer 4" className="w-full px-4 py-2 transition duration-300 border border-gray-300 rounded focus:border-transparent focus:outline-none focus:ring-4 focus:ring-green-400" />
-                                <button className="rounded-full p-2 w-10 hover:bg-green-400 text-white"> &#10004; </button>
+                                <button className="rounded-full p-2 w-10 hover:bg-green-400 text-gray-200"> <FontAwesomeIcon icon={faCheck}></FontAwesomeIcon> </button>
                             </div>
                         </div>
                     </form>
                 </div>
-                <div className="bg-red-300 h-32 my-2 p-2">Button sound</div>
             </div >
         </section >
     )
