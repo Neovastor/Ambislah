a959e825f29132f0edc5bc890a94889982dc976a
HOGAKE-ke-7 <adiisrahadi@gmail.com>
2021-07-24 16:53:02 +0700

    cek

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -16,16 +16,7 @@ export default function Report() {
                     <div className="bg-white shadow-md rounded my-6">
 
                       <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-gray-300 border-0">
-                        <div className="rounded-t mb-0 px-6 py-6">
-                            <div className="text-center mb-3">
-                                <h6 className="text-gray-600 text-sm font-bold">Sign in with</h6></div>
-                            <div className="btn-wrapper text-center">
-                                <button className="bg-white active:bg-gray-100 text-gray-800 font-normal px-4 py-2 rounded outline-none focus:outline-none mr-1 mb-1 uppercase shadow hover:shadow-md inline-flex items-center font-bold text-xs" type="button" style={{"transition": "all 0.15s ease 0s"}}><img alt="..." className="w-5 mr-1" src="https://demos.creative-tim.com/tailwindcss-starter-project/static/media/google.87be59a1.svg" />Google</button>
-                            </div>
-                            <hr className="mt-6 border-b-1 border-gray-400" />
-                        </div>
                         <div className="flex-auto px-4 lg:px-10 py-10 pt-0">
-                            <div className="text-gray-500 text-center mb-3 font-bold"><small>Or sign in with credentials</small></div>
                             <form>
                                 <div className="relative w-full mb-3">
                                     <label className="block uppercase text-gray-700 text-xs font-bold mb-2" for="grid-password">Email</label>
@@ -44,6 +35,14 @@ export default function Report() {
                                 </div>
                             </form>
                             </div>
+                        </div>
+                        <div className="rounded-t mb-0 px-6 py-6">
+                            <div className="text-center mb-3">
+                                <h6 className="text-gray-600 text-sm font-bold">Sign in with</h6></div>
+                            <div className="btn-wrapper text-center">
+                                <button className="bg-white active:bg-gray-100 text-gray-800 font-normal px-4 py-2 rounded outline-none focus:outline-none mr-1 mb-1 uppercase shadow hover:shadow-md inline-flex items-center font-bold text-xs" type="button" style={{"transition": "all 0.15s ease 0s"}}><img alt="..." className="w-5 mr-1" src="https://demos.creative-tim.com/tailwindcss-starter-project/static/media/google.87be59a1.svg" />Google</button>
+                            </div>
+                            <hr className="mt-6 border-b-1 border-gray-400" />
                         </div>
                       </div>
                       
