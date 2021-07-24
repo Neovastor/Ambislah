5b1fc0f9a6b192c53204e96dd18d982bbcc6ec5b
HOGAKE-ke-7 <adiisrahadi@gmail.com>
2021-07-24 13:14:56 +0700

    testing web RTC

---
+++ server/app.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express()
+const http = require('http')
+const server = http.createServer(app)
+const socket = require('socket.io')
+const io = socket(server)
+const cors = require('cors')
+
+io.on('connection', (socket) => {
+    socket.emit('me', socket.id)
+
+    socket.on('disconnect', () => {
+        socket.broadcast.emit('callEnded')
+    })
+
+    socket.on('callUser', (data) => {
+        io.to(data.userToCall).emit('callUser', { signal: data.signalData, from: data.from, name: data.name })
+    })
+
+    socket.on('answerCall', (data) => io.to(data.to).emit('callAccept'), data.signal)
+})
+
+server.use(cors())
+server.listen(5000, () => {
+    console.log('server ready at http://localhost:5000');
+})
