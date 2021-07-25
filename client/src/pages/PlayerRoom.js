9da2fa52bf12dab5b1ab16cb97bfb6404eb3e034
HOGAKE-ke-7 <adiisrahadi@gmail.com>
2021-07-25 17:55:53 +0700

    ok

---
+++ client/src/pages/PlayerRoom.js
@@ -0,0 +1,8 @@
+
+function PlayerRoom(){
+    return (
+        <h1>Waiting host to start the game</h1>
+    )
+}
+
+export default PlayerRoom
--- orchestrator/axios/index.js
+++
@@ -1,9 +0,0 @@
-const axios = require('axios')
-
-const axiosQuizzes = axios.create({
-    baseURL: `http://localhost:4001/quizzes`
-})
-
-module.exports = {
-    axiosQuizzes,
-}
