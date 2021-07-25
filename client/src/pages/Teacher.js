7d3a4277b5d77233043831c4de02c09481410196
HOGAKE-ke-7 <adiisrahadi@gmail.com>
2021-07-25 19:13:05 +0700

    fadil

--- client/src/pages/Teacher.js
+++ client/src/pages/Teacher.js
@@ -6,103 +6,53 @@ import { Link, useHistory } from 'react-router-dom';
 import { GOOGLE_LOGIN, LOGIN } from '../graphql/queiries/userQueries';
 import { useAlert } from 'react-alert';
 import Swal from 'sweetalert2'
-import { loginVar } from "../graphql/vars";
+import { answerVar } from "../graphql/vars";
 
 export default function Report() {
   const { register, handleSubmit } = useForm();
-  const isLogin = useReactiveVar(loginVar)
+  const answer = useReactiveVar(answerVar)
   const alert = useAlert()
   const history= useHistory()
   const [login, { data: datalogin }] = useMutation(LOGIN)
   const [googlelogin, { data: datagooglelogin }] = useMutation(GOOGLE_LOGIN)
 
-  const CALLBACK = async (response) => {
-    try {
-      console.log(response);
-      console.log('id_token',response.tokenId)
-      const res = await googlelogin({
-        variables: {
-          input: {
-            id_token: response.tokenId
-          }
-        }
-      })
-      console.log('>>>>>>', res.data.googlelogin)
-      localStorage.setItem('access_token', res.data.googlelogin.access_token)
-      history.push('/')
-      // alert.success('Welcome')
-      loginVar(true)
-      Swal.fire({
-        icon: "success",
-        title: `Welcome..  ${response.Os.Ne}!`,
-        imageUrl: `${response.profileObj.imageUrl}`,
-        imageWidth: 200,
-        imageHeight: 100,
-        imageAlt: 'Custom image',
-        showConfirmButton: false,
-        timer: 2500,
-      });
-    } catch (err) {
-      Swal.fire({
-        icon: 'error',
-        title: 'Oops...',
-        text: 'Something wrong!',
-        footer: '<h1>Wanna try again?</h1>'
-      })
-    }
-  }
-  const submitLogin = async e => {
-    try {
-      const { email, password } = e
-      const res = await login({
-        variables: {
-          input: {
-            email: email,
-            password: password
-          }
-        }
-      })
-      console.log('>>>>>>', res.data.login)
-      localStorage.setItem('access_token', res.data.login.access_token)
-      history.push('/')
-      // alert.success('Welcome')
-      loginVar(true)
-      Swal.fire({
-        icon: "success",
-        title: "Welcome..!",
-        showConfirmButton: false,
-        timer: 1500,
-      });
-    } catch (err) {
-      Swal.fire({
-        icon: 'error',
-        title: 'Oops...',
-        text: 'Username and Password did not match!',
-        footer: '<h1>Wanna try again?</h1>'
-      })
-    }
-  }
-    return (
-      <div className="overflow-x-auto pt-14">
-          <div className="min-w-screen min-h-[777px] bg-gray-100 flex items-center justify-center font-sans overflow-hidden">
-            <div className="w-full lg:w-5/6 max-w-screen-2xl pt-5">
-              <div className="bg-white shadow-md rounded-lg my-6 min-h-screen">
-                <div className="flex justify-center">
-                  <input type="text" className="text px-72 py-8 bg-gray-400 focus:ring-2 focus:ring-blue-500 focus:outline-none rounded-xl" />
+  return (
+    <div className="overflow-x-auto pt-14">
+        <div className="min-w-screen min-h-[777px] bg-gray-100 flex items-center justify-center font-sans overflow-hidden">
+          <div className="w-full lg:w-5/6 max-w-screen-2xl pt-5">
+            <div className="bg-white shadow-md rounded-lg my-6 min-h-screen">
+              <div className="flex justify-center">
+                <input type="text" className="text px-72 py-8 bg-gray-400 focus:ring-2 focus:ring-blue-500 focus:outline-none rounded-xl" />
+              </div>
+              <div className="flex justify-center">
+                <div className="flex flex-col mx-60 my-60">
+                <button className="px-6 py-4 rounded-lg bg-red-500 min-w-[40px] my-4">
+                  <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M11 5.882V19.24a1.76 1.76 0 01-3.417.592l-2.147-6.15M18 13a3 3 0 100-6M5.436 13.683A4.001 4.001 0 017 6h1.832c4.1 0 7.625-1.234 9.168-3v14c-1.543-1.766-5.067-3-9.168-3H7a3.988 3.988 0 01-1.564-.317z" />
+                  </svg>
+                </button>
                 </div>
-                <div className="flex justify-center">
-                  <div className="flex flex-col mx-60 my-60">
-                  <button className="px-6 py-4 rounded-lg bg-red-500 min-w-[400px] my-4">aaaaa</button>
-                  <button className="px-6 py-4 rounded-lg bg-red-500 min-w-[400px] my-4">bbbbb</button>
-                  </div>
-                  <div className="flex flex-col mx-60 my-60">
-                  <button className="px-6 py-4 rounded-lg bg-red-500 min-w-[400px] my-4">ccccc</button>
-                  <button className="px-6 py-4 rounded-lg bg-red-500 min-w-[400px] my-4">ddddd</button>
-                  </div>
+                <div className="flex flex-col mx-60 my-60">
+                <button className="px-6 py-4 rounded-lg bg-red-500 min-w-[40px] my-4">
+                  <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z" />
+                  </svg>
+                </button>
+                </div>
+              </div>
+              <div className="flex justify-center">
+                <div className="flex flex-col mx-60 my-60">
+                <button className="px-6 py-4 rounded-lg bg-red-500 min-w-[400px] my-4">aaaaa</button>
+                <button className="px-6 py-4 rounded-lg bg-red-500 min-w-[400px] my-4">bbbbb</button>
+                </div>
+                <div className="flex flex-col mx-60 my-60">
+                <button className="px-6 py-4 rounded-lg bg-red-500 min-w-[400px] my-4">ccccc</button>
+                <button className="px-6 py-4 rounded-lg bg-red-500 min-w-[400px] my-4">ddddd</button>
                 </div>
               </div>
             </div>
-        </div>
+          </div>
       </div>
-    )
+    </div>
+  )
 }
