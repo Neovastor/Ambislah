34cf50743178df649dfb2f92fefe395a65552081
HOGAKE-ke-7 <adiisrahadi@gmail.com>
2021-07-25 15:41:40 +0700

    rev.03 - footer component

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import './styles/output.css'
 import { Switch, Route } from 'react-router-dom'
 import Home from './pages/Home';
 import NavBar from './components/NavBar';
-import Footer from './pages/Footer';
+import Footer from './components/Footer';
 import Create from './pages/Create';
 import Report from './pages/Report';
 import Login from './pages/Login'
