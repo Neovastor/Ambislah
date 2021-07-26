2693a46cff3817a955da3084a7be07f3043196f0
Neovastor <neovastr1997@gmail.com>
2021-07-26 22:31:26 +0700

    merge brian with dev

--- server/services/models/quizModel.js
+++ server/services/models/quizModel.js
@@ -1,5 +1,28 @@
 const { getDatabase, ObjectId } = require('../config/mongodb')
 
+const cron = require('node-cron')
+
+function cronJob (day, roomId) {
+  // const expression = '*/2 * * * * *'
+  // const expression = '* * 72 * * *'
+  // const expression = '* * * * * *'
+  // const hour = day * 24
+  // const expression = `* */${hour} * * * *`
+  const hour = day
+  const expression = `*/${hour} * * * * *`
+  const task = cron.schedule(expression, () => {
+    console.log(`del-ROOM-ID ${roomId}`)
+    // jika room id ada maka lakukan hapus
+  }, {
+    scheduled: false,
+    timezone: 'Asia/Jakarta'
+  })
+  task.start()
+}
+
+cronJob(1, 'satu detik')
+cronJob(5, 'LIMAA')
+
 class Quizzes {
     static async findAll(userId) {
         const quizzesCollection = getDatabase().collection('Quizzes')
