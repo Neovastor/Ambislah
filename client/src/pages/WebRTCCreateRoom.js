008b0e9ba285cd15f3bbc2b24652166bd9b00113
HOGAKE-ke-7 <adiisrahadi@gmail.com>
2021-07-26 08:55:40 +0700

    ok

--- client/src/pages/WebRTCCreateRoom.js
+++ client/src/pages/WebRTCCreateRoom.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { v1 as uuid } from "uuid";
+import { useHistory } from 'react-router-dom';
 
-const CreateRoom = (props) => {
+const CreateRoom = () => {
+  const history = useHistory()
   function create() {
       const id = uuid();
-      props.history.push(`/room/${id}`);
+      history.push(`/room/${id}`);
   }
 
   return (
