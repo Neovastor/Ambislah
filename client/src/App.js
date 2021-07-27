f89b101e318953bcecf27fef5b065ef9756edfae
Neovastor <neovastr1997@gmail.com>
2021-07-27 21:57:18 +0700

    success hadi and brian

--- client/src/App.js
+++ client/src/App.js
@@ -6,17 +6,12 @@ import CreateRoom from "./pages/CreateRoom";
 import Room from "./pages/Room";
 import Login from './pages/Login'
 import Register from './pages/Register'
-import Teacher from './pages/Teacher'
-import WebRTCCreateRoom from './pages/WebRTCCreateRoom'
-import WebRTCRoom from './pages/WebRTCRoom'
 import Home from './pages/Home';
 import NavBar from './components/NavBar';
 import Footer from './components/Footer';
 import Create from './pages/Create';
 import Report from './pages/Report';
 
-import Login from "./pages/Login";
-import Register from "./pages/Register";
 
 import Host from "./pages/Host";
 import WaitingRoom from "./pages/HostRoom";
@@ -49,9 +44,6 @@ function App() {
         <Route exact path="/register"> <Register /> </Route>
         <Route exact path="/report"> <Report /> </Route>
         <Route exact path="/create"> <Create /> </Route>
-        <Route exact path="/teacher"> <Teacher /> </Route>
-        <Route exact path="/room2"> <WebRTCCreateRoom /> </Route>
-        <Route exact path="/room2/:roomID"> <WebRTCRoom /> </Route>
         <Route path="/room" exact component={CreateRoom} />
         <Route path="/room/:roomID" component={Room} />
           <Route exact path="/host">            
