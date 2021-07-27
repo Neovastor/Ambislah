8cef58417465cf7e00c9939a6280ca4a7c32e9f8
HOGAKE-ke-7 <adiisrahadi@gmail.com>
2021-07-27 10:26:07 +0700

    masuk MOBILE app

--- zxclient/src/graphql/vars.js
+++
@@ -1,4 +0,0 @@
-import { makeVar } from '@apollo/client'
-
-export const loginVar = makeVar(false)
-export const answerVar = makeVar('')
--- zxclient/src/pages/PlayerRoom.js
+++
@@ -1,8 +0,0 @@
-
-function PlayerRoom(){
-    return (
-        <h1>Waiting host to start the game</h1>
-    )
-}
-
-export default PlayerRoom
