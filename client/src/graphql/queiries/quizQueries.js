b9b3e34cfbce2070ae4943f53cd99d738bfeec7d
HOGAKE-ke-7 <adiisrahadi@gmail.com>
2021-07-28 13:33:06 +0700

    jos

---
+++ client/src/graphql/queiries/quizQueries.js
@@ -0,0 +1,72 @@
+import { gql } from '@apollo/client'
+
+export const GET_ALL_QUIZ = gql`
+query Query {
+  Quizzes {
+    _id
+    userId
+    title
+    questions {
+      type
+      question
+      image
+      choose
+      answer
+    }
+    timer
+    mode
+    createdAt
+  }
+}
+`
+
+export const CREATED_QUIZZES = gql`
+ query createdQuiz{
+  createdQuiz @client
+ }
+`
+
+export const ADD_QUIZZES = gql`
+mutation AddQuizzesMutation($addQuizzesUserId: String, $addQuizzesTitle: String, $addQuizzesQuestions: [InputQuestion], $addQuizzesTimer: Int, $addQuizzesMode: String, $addQuizzesCreatedAt: Date) {
+  AddQuizzes(userId: $addQuizzesUserId, title: $addQuizzesTitle, questions: $addQuizzesQuestions, timer: $addQuizzesTimer, mode: $addQuizzesMode, createdAt: $addQuizzesCreatedAt) {
+    _id
+    userId
+    title
+    questions {
+      type
+      question
+      image
+      choose
+      answer
+    }
+    timer
+    mode
+    createdAt
+  }
+}
+`
+export const DELETE_QUIZZEZ = gql`
+mutation DeleteQuizzesByIdMutation($deleteQuizzesByIdId: ID) {
+  DeleteQuizzesById(id: $deleteQuizzesByIdId)
+}
+`
+
+export const UPDATE_QUIZZES = gql`
+mutation Mutation($editQuizzesByIdId: ID, $editQuizzesByIdUserId: String, $editQuizzesByIdTitle: String, $editQuizzesByIdQuestions: [InputQuestion], $editQuizzesByIdTimer: Int, $editQuizzesByIdMode: String, $editQuizzesByIdCreatedAt: Date) {
+  EditQuizzesById(id: $editQuizzesByIdId, userId: $editQuizzesByIdUserId, title: $editQuizzesByIdTitle, questions: $editQuizzesByIdQuestions, timer: $editQuizzesByIdTimer, mode: $editQuizzesByIdMode, createdAt: $editQuizzesByIdCreatedAt) {
+    _id
+    userId
+    title
+    questions {
+      type
+      question
+      image
+      choose
+      answer
+    }
+    timer
+    mode
+    createdAt
+  }
+}
+`
