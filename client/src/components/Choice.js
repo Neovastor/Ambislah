317313ea367b39a4011b1398cdc6d753708a5034
HOGAKE-ke-7 <adiisrahadi@gmail.com>
2021-07-28 23:51:09 +0700

    cleaning milionaire

--- client/src/components/Choice.js
+++ client/src/components/Choice.js
@@ -4,11 +4,6 @@ function Choice({ choice }) {
       <img className="top-0 object-fill" src="/mil-but.png" alt=".." /> 
       <button className="absolute top-0 h-11 rounded-lg p-2 transform translate-x-[100px] translate-y-[25px] text-xl bg-transparant hover:bg-red-600 text-gray-200"> {choice} </button>
     </div>
-    // <div className="flex">
-    //   <button className="h-20 rounded-lg p-2 bg-red-500 hover:bg-red-600 w-screen text-gray-200">
-    //     {choice}
-    //   </button>
-    // </div>
   );
 }
 
--- client/src/components/Question.js
+++ client/src/components/Question.js
@@ -38,22 +38,6 @@ function Question({ question, i,duration }) {
             {question.choose.map((choice, j) => {
               return <Choice choice={choice} key={j} />;
             })}   
-              {/* <div className="relative">
-                <img className="top-0 object-fill" src="/mil-but.png" alt=".." /> 
-                <button className="absolute top-0 h-11 rounded-lg p-2 transform translate-x-[100px] translate-y-[25px] text-xl bg-transparant hover:bg-red-600 text-gray-200"> {question.choose[0]} </button>
-              </div>
-              <div className="relative">
-                <img className="top-0 object-fill" src="/mil-but.png" alt=".." /> 
-                <button className="absolute top-0 h-11 rounded-lg p-2 transform translate-x-[100px] translate-y-[25px] text-xl bg-transparant hover:bg-red-600 text-gray-200"> {question.choose[1]} </button>
-              </div>
-              <div className="relative">
-                <img className="top-0 object-fill" src="/mil-but.png" alt=".." /> 
-                <button className="absolute top-0 h-11 rounded-lg p-2 transform translate-x-[100px] translate-y-[25px] text-xl bg-transparant hover:bg-red-600 text-gray-200"> {question.choose[2]} </button>
-              </div>
-              <div className="relative">
-                <img className="top-0 object-fill" src="/mil-but.png" alt=".." /> 
-                <button className="absolute top-0 h-11 rounded-lg p-2 transform translate-x-[100px] translate-y-[25px] text-xl bg-transparant hover:bg-red-600 text-gray-200"> {question.choose[3]} </button>
-              </div> */}
           </div>
 
           <div>
