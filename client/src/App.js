c101d9a42bc9cdc09f30e723cfb7945a5c73ccf2
HOGAKE-ke-7 <adiisrahadi@gmail.com>
2021-07-28 17:18:55 +0700

    ok

--- client/src/App.js
+++ client/src/App.js
@@ -6,11 +6,15 @@ import CreateRoom from "./pages/CreateRoom";
 import Room from "./pages/Room";
 import Login from './pages/Login'
 import Register from './pages/Register'
+import Report from './pages/Report';
 import Home from './pages/Home';
+import Collections from './pages/Collections'
+// import WaitingRoom from './pages/WaitingRoom'
+import UpdateQuiz from './pages/UpdateQuiz'
+import UpdateQuestions from './pages/UpdateQuestions'
 import NavBar from './components/NavBar';
 import Footer from './components/Footer';
 import Create from './pages/Create';
-import Report from './pages/Report';
 
 
 import Host from "./pages/Host";
@@ -40,11 +44,16 @@ function App() {
     <>
       <NavBar></NavBar>
       <Switch>
+
+        <Route exact path="/collections/updatequestion"> <UpdateQuestions /> </Route>
+        <Route exact path="/collections/update"> <UpdateQuiz /> </Route>
+        <Route exact path="/waitingroom"> <WaitingRoom /> </Route>
+        <Route exact path="/collections"> <Collections /> </Route>
         <Route exact path="/login"> <Login /> </Route>
         <Route exact path="/register"> <Register /> </Route>
         <Route exact path="/report"> <Report /> </Route>
         <Route exact path="/create"> <Create /> </Route>
-        <Route exact path="/room"  component={CreateRoom} />
+        <Route path="/room" exact component={CreateRoom} />
         <Route path="/room/:roomID" component={Room} />
           <Route exact path="/host">            
             <Host db={db}></Host>
@@ -59,12 +68,12 @@ function App() {
           </Route>
 
           <Route exact path="/player/:idroom">
-            
             <PlayerRoom db={db}></PlayerRoom>
           </Route>
         <Route> <Home /> </Route>
           
       </Switch>
+      <Footer></Footer>
     </>
   )
 
