ebe39b0d333e2d9e8474479d765f6625e784d1b3
HOGAKE-ke-7 <adiisrahadi@gmail.com>
2021-07-28 21:42:57 +0700

    ok

--- client/src/pages/HostRoom.js
+++ client/src/pages/HostRoom.js
@@ -337,7 +337,7 @@ function WaitingRoom({ db }) {
         >
           <div className="grid grid-cols-2">
             <div className=" flex flex-col justify-center items-center max-h-[555px] mt-[200px]">
-              <div className="bg-green-600 rounded-lg pb-4">
+              <div className="bg-green-600 rounded-lg pb-4 mb-7">
                 <div className="m-4 flex justify-center">
                   <button className="rounded-full text-xl font-bold px-6 py-4 mx-7 text-white bg-red-500">
                     your PIN
@@ -356,12 +356,22 @@ function WaitingRoom({ db }) {
                   </button>
                 </div>
               </div>
-              <div className="m-4 ">
-                <h2>Peserta</h2>
-                <div className="m-4  ">
-                  {players.length > 0 ? (
-                    <PlayerTable players={players} />
-                  ) : null}
+              <div className="bg-green-600 rounded-lg pb-4 mt-4 min-w-[400px]">
+                <div className="m-4">
+                  <div className="flex flex-row text-white font-extrabold text-xl">
+                    <h2 className="flex">
+                      <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" > <path d="M12 14l9-5-9-5-9 5 9 5z" /> <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z" /> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222" /> </svg>
+                      <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" > <path d="M12 14l9-5-9-5-9 5 9 5z" /> <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z" /> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222" /> </svg>
+                        Participants
+                      <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" > <path d="M12 14l9-5-9-5-9 5 9 5z" /> <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z" /> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222" /> </svg>
+                      <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" > <path d="M12 14l9-5-9-5-9 5 9 5z" /> <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z" /> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222" /> </svg>
+                    </h2>
+                  </div>
+                  <div className="m-4  ">
+                    {players.length > 0 ? (
+                      <PlayerTable players={players} />
+                    ) : null}
+                  </div>
                 </div>
               </div>
             </div>
