ceddb1958755562670cdb8c7a9d066c216251010
HOGAKE-ke-7 <adiisrahadi@gmail.com>
2021-07-29 00:16:01 +0700

    ok

--- client/src/components/PlayerTable.js
+++ client/src/components/PlayerTable.js
@@ -6,7 +6,7 @@ function PlayerTable({players}){
           {players.map((player, i) => {
             return (
               <li key={2000 + i}>
-                <div className="max-w-[350px] mx-4 flex flex-col justify-center items-center">
+                <div className="max-w-[350px] mx-4 flex flex-col justify-center items-center text-black">
                   <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor"> <path fillRule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clipRule="evenodd" /> </svg>
                   {player.playername.substring(0,7)}
                 </div>
