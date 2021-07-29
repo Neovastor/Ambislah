d531a983a24c697eceb5b6d91d1eaaf6223b9984
HOGAKE-ke-7 <adiisrahadi@gmail.com>
2021-07-29 20:17:38 +0700

    ok

--- client/src/pages/Leaderboard.js
+++ client/src/pages/Leaderboard.js
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import { useHistory } from "react-router-dom";
 import { useMutation } from '@apollo/client'
 import { ADD_REPORT } from "../graphql/queiries";
+
 function Leaderboard({ db, idparams }) {
   const [addReport] = useMutation(ADD_REPORT)
   const [leaderboard, setLeaderboard] = useState([]);
@@ -25,7 +26,7 @@ function Leaderboard({ db, idparams }) {
           }
           return 0;
         });
-      }else {
+      } else {
         sortedLeaderboard = doc.data().leaderboard;
       }
       setLeaderboard(sortedLeaderboard);
@@ -33,7 +34,7 @@ function Leaderboard({ db, idparams }) {
   }, []);
 
   function finishHandler(e) {
-    db.collection("quizzes").onSnapshot( async (querySnapshot) => {
+    db.collection("quizzes").onSnapshot(async (querySnapshot) => {
       const data = querySnapshot.docs.map((doc) => ({
         ...doc.data(),
       }));
@@ -41,8 +42,8 @@ function Leaderboard({ db, idparams }) {
       const choosenQuiz = data.find(({ roomkey }) => +roomkey === +idparams);
 
       let payload = {
-        // playersCount: livegamesData.players.length,
-        // date: new Date(),
+        playersCount: livegamesData.players.length,
+        date: new Date(),
         players: livegamesData.leaderboard,
         quizTitle: choosenQuiz.title,
         quizId: choosenQuiz.id,
@@ -86,59 +87,57 @@ function Leaderboard({ db, idparams }) {
   }
 
   return (
-    <div className="overflow-x-auto pt-14">
-      <h1 className="text-center font-extrabold text-7xl bg-transparent">Leaderboard</h1>
-      <div className="min-w-screen min-h-screen bg-gray-100 flex items-center justify-center font-sans overflow-hidden">
-        {leaderboard.length > 0 ? (
-          <div className="w-full lg:w-5/6 pt-5">
-            <div className="bg-white shadow-md rounded my-6">
-              <table className="min-w-max w-full table-auto">
-                <thead>
-                  <tr className="bg-gray-200 text-gray-600 uppercase text-sm leading-normal">
-                    <th className="py-3 px-6 text-left">Number</th>
-                    <th className="py-3 px-6 text-left">Name</th>
-                    <th className="py-3 px-6 text-center">Score</th>
-                  </tr>
-                </thead>
-                <tbody className="text-gray-600 text-sm font-light">
-                  {leaderboard.map((row, index) => {
-                    return (
-                      <tr
-                        key={index}
-                        className="border-b border-gray-200 hover:bg-gray-100"
-                      >
-                        <td className="py-3 px-6 text-left whitespace-nowrap">
-                          <div className="flex items-center">
-                            <span className="font-medium">{index + 1}</span>
-                          </div>
-                        </td>
-                        <td className="py-3 px-6 text-left bg-yellow-300">
-                          <div className="flex items-center">
+    <div className="bg-[#27729e] min-h-screen mmd:h-full flex flex-auto justify-center items-center">
+      <div className="bg-[#429dda] min-h-screen w-[90%] mmd:pt-8 mt-20">
+        {leaderboard.length > 0 ? (<>
+          <h1 className="text-center font-extrabold text-7xl bg-transparent m-4 text-white uppercase">Leaderboard</h1>
+          <div className="bg-white shadow-md rounded m-6">
+            <table className="min-w-max w-full table-auto">
+              <thead>
+                <tr className="bg-gray-200 text-gray-600 uppercase text-sm leading-normal">
+                  <th className="py-3 px-6 text-left">Number</th>
+                  <th className="py-3 px-6 text-left">Name</th>
+                  <th className="py-3 px-6 text-center">Score</th>
+                </tr>
+              </thead>
+              <tbody className="text-gray-600 text-sm font-light">
+                {leaderboard.map((row, index) => {
+                  return (
+                    <tr
+                      key={index}
+                      className="border-b border-gray-200 hover:bg-gray-100"
+                    >
+                      <td className="py-3 px-6 text-left whitespace-nowrap">
+                        <div className="flex items-center">
+                          <span className="font-medium">{index + 1}</span>
+                        </div>
+                      </td>
+                      <td className="py-3 px-6 text-left bg-white">
+                        <div className="flex items-center">
 
-                            <span>{row.name}</span>
-                          </div>
-                        </td>
-                        <td className="py-3 px-6 text-center">
-                          <span className=" py-1 px-3 rounded-full text-xs">
-                            {row.score}
-                          </span>
-                        </td>
-                      </tr>
-                    );
-                  })}
-                </tbody>
-              </table>
-            </div>
+                          <span>{row.name}</span>
+                        </div>
+                      </td>
+                      <td className="py-3 px-6 text-center">
+                        <span className=" py-1 px-3 rounded-full text-xs">
+                          {row.score}
+                        </span>
+                      </td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
           </div>
+        </>
         ) : null}
-        <div className="grid">
-          <button className="px-6 py-4 rounded-full bg-yellow-500 text-white hover:bg-red-500 hover:text-red-300" onClick={(e) => finishHandler(e)}>
+        <div className="flex justify-end m-6">
+          <button className="px-8 rounded-lg bg-[#053742] font-bold p-4 uppercase hover:border-[#053742] hover:bg-[#053742] text-white border-2" onClick={(e) => finishHandler(e)}>
             Finish
           </button>
         </div>
       </div>
- 
-    </div>
+    </div >
   );
 }
 
